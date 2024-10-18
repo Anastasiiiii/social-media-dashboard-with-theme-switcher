a7670a4e3082881d6889981ae1826b2d4c5b30c3
anastasiiiii <nastia262002@gmail.com>
2024-10-18 21:47:23 +0200

    change files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import useLocalStorage from "../useLocalStorage"
 import "../styles/Formulas.css";
 import "../styles/Header.css";
